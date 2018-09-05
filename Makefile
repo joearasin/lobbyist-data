6a75b6974ff20c3694841114fb4e833131ccec1b
Joe Arasin <joe@arasin.com>
2018-09-05 19:56:40 -0400

    Add Issues, handle errors

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
+.DELETE_ON_ERROR:
+	
+clean:
+	rm -rf output/*
+
 data/files/house/%_XML.zip:
 	mkdir -p $(dir $@)
 	./house_fetcher.py download --file $(notdir $(basename $@)) > $@
 
 
-data/processed/house/%_Registrations_Records.csv: data/files/house/%_Registrations_XML.zip
+output/house/%_Registrations_Records.csv: data/files/house/%_Registrations_XML.zip
 	mkdir -p $(dir $@)
 	./house_processor.py registrations $< > $@
 
-data/processed/house/%_Registrations_Lobbyists.csv: data/files/house/%_Registrations_XML.zip
+output/house/%_Registrations_Lobbyists.csv: data/files/house/%_Registrations_XML.zip
 	mkdir -p $(dir $@)
 	./house_processor.py lobbyists $< > $@
+
+output/house/%_Registrations_Issues.csv: data/files/house/%_Registrations_XML.zip
+	mkdir -p $(dir $@)
+	./house_processor.py issues $< > $@
--- house_processor.py
+++ house_processor.py
@@ -22,10 +22,15 @@ Registration = namedtuple('Registration', REGISTRATION_COLUMNS)
 
 class HouseRegistrationsFile():
     def __init__(self, contents):
-                if path.isfile(contents):
-                    self.obj = objectify.parse(contents).getroot()
-                else:
-                    self.obj = objectify.fromstring(contents)
+        if path.isfile(contents):
+            obj = objectify.parse(contents).getroot()
+        else:
+            obj = objectify.fromstring(contents)
+        if("LOBBYINGDISCLOSURE1" in obj.tag):
+            # Match on substring b/c some documents have a namespace
+            self.obj = obj
+        else:
+            raise ValueError("Document is not an LOBBYINGDISCLOSURE1. It is a {}".format(obj.tag))
 
     def lobbyists(self):
         lobbyists = self.obj.lobbyists
@@ -45,6 +50,12 @@ class HouseRegistrationsFile():
             get_column('city'),get_column('state'),get_column('zip'),get_column('zipext'),get_column('country'),get_column('principal_city'),get_column('principal_state'), get_column('principal_zip'), get_column('principal_zipext'), get_column('principal_country'), get_column('contactIntlPhone'), get_column('registrantGeneralDescription'), get_column('selfSelect'), get_column('clientName'), get_column('clientAddress'), get_column('clientCity'), get_column('clientState'), get_column('clientZip'), get_column('clientZipExt'), get_column('clientCountry'), get_column('prinClientCity'), get_column('prinClientState'), get_column('prinClientZip'), get_column('prinClientZipExt'), get_column('prinClientCountry'), get_column('clientGeneralDescription'), get_column('senateID'), get_column('houseID'), get_column('specific_issues'), get_column('affiliatedUrl'), get_column('reportYear'), get_column('reportType'), get_column('effectiveDate'), get_column('printedName'), get_column('signedDate')
         )
 
+    def issues(self):
+        alis = self.obj.alis
+        if(hasattr(alis, 'ali_Code')):
+            return [ str(ali_code).strip() for ali_code in alis.ali_Code if str(ali_code).strip()]
+        return []
+
 
 def read_files(files):
         for file in files:
@@ -58,15 +69,22 @@ def read_files(files):
             else:
                 yield Path(file).stem, file
 
+def read_registrations(files):
+    for (file_id, contents) in read_files(files):
+        try:
+            reg_file = HouseRegistrationsFile(contents)
+            yield file_id, reg_file
+        except ValueError as err:
+            print("Could not read {}. Error: {}".format(file_id, err), file=sys.stderr)
+
 @cli.command()
 @click.argument('files', nargs=-1, type=click.Path())
 def registrations(files):
     COLUMNS = ['id'] + REGISTRATION_COLUMNS
     data = tablib.Dataset(headers=COLUMNS)
     data = tablib.Dataset(headers=COLUMNS)
-    for (file_id, contents) in read_files(files):
-        reg_file = HouseRegistrationsFile(contents)
-        data.append([file_id] + list(reg_file.registration()))
+    for (file_id, registration) in read_registrations(files):
+        data.append([file_id] + list(registration.registration()))
     sys.stdout.buffer.write(data.export('csv').encode())
 
 
@@ -75,10 +93,19 @@ def registrations(files):
 def lobbyists(files):
     COLUMNS = ['registration_id'] + LOBBYIST_COLUMNS
     data = tablib.Dataset(headers=COLUMNS)
-    for (file_id, contents) in read_files(files):
-        reg_file = HouseRegistrationsFile(contents)
-        for lobbyist in reg_file.lobbyists():
+    for (file_id, registration) in read_registrations(files):
+        for lobbyist in registration.lobbyists():
             data.append([file_id] + list(lobbyist))
+    sys.stdout.buffer.write(data.export('csv').encode())
+
+@cli.command()
+@click.argument('files', nargs=-1, type=click.Path())
+def issues(files):
+    COLUMNS = ['registration_id', 'ali_code']
+    data = tablib.Dataset(headers=COLUMNS)
+    for (file_id, registration) in read_registrations(files):
+        for issue in registration.issues():
+            data.append([file_id, issue])
     sys.stdout.buffer.write(data.export('csv').encode())
 
 if __name__ == '__main__':
